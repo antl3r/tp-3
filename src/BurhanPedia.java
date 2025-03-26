3ee80263b1a2d3357ae8a0d2fee5dfc1bd6bc920
sergei <adrian.illumina@gmail.com>
2025-03-26 11:05:52 +0700

    first commit

---
+++ src/BurhanPedia.java
@@ -0,0 +1,31 @@
+import repos.AdminRepository;
+import repos.PromoRepository;
+import repos.UserRepository;
+import repos.VoucherRepository;
+
+public class BurhanPedia {
+    private UserRepository userRepo = new UserRepository();
+    private VoucherRepository voucherRepo = new VoucherRepository();
+    private PromoRepository promoRepo = new PromoRepository();
+    private AdminRepository adminRepo = new AdminRepository();
+
+    public UserRepository getUserRepo() {
+        return userRepo;
+    }
+
+    public VoucherRepository getVoucherRepo() {
+        return voucherRepo;
+    }
+
+    public PromoRepository getPromoRepo() {
+        return promoRepo;
+    }
+
+    public AdminRepository getAdminRepo() {
+        return adminRepo;
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
