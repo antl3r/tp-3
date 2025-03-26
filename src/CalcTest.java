eb4c2ce6f14109170ea5bee32ad2fe6d6d084f0c
sergei <adrian.illumina@gmail.com>
2025-03-26 12:15:11 +0700

    X

--- src/CalcTest.java
+++ src/CalcTest.java
@@ -1,11 +1,11 @@
-import org.junit.Test;
-import org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
 
 public class CalcTest {
     @Test
     public void Test1() {
-        int result = 1;
+        int result = 2 - 1;
         int expected = 1;
         assertEquals(expected,result);
     }
